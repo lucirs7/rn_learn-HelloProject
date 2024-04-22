af746a6f0a9e3d7a21c00d2b4ba3fb32ac329d74
lucirs7 <luciars787@gmail.com>
2024-04-22 20:25:05 +0200

    Video #14. Using component 'image'.
     Placed an image from inside project and another image from a website.

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Image } from "react-native";
 import { NameText } from "./src/components/nameText";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <NameText nameValue="Lucy" />
+      <Image source={require('./src/images/email.png')} />
+      <Image source={{
+        uri: "https://images.pexels.com/photos/20552024/pexels-photo-20552024/free-photo-of-mar-amanecer-paisaje-puesta-de-sol.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+      }}
+        style={styles.image} />
     </View>
   );
 }
@@ -16,5 +21,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#EA7773',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  image: {
+    width: '85%',
+    height: '50%',
   },
 });
