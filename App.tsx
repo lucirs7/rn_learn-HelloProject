8e42a5f8f26da0dcc986d0c1cbf0f6425aa6196f
lucirs7 <luciars787@gmail.com>
2024-05-02 17:43:48 +0200

    Videos #33 and #34. Added third party component.
    Installed native-base and added a button.

--- App.tsx
+++ App.tsx
@@ -1,32 +1,17 @@
-import React, { useState } from "react";
-import { Text, View, StyleSheet, Alert, TouchableOpacity } from "react-native";
+import React from "react";
+import { Text, View, StyleSheet} from "react-native";
+import { NativeBaseProvider, Button } from "native-base";
 
 export default function App() {
-  //const [myText, setMyText] = useState('Generate random colour');
-
-  const generateRandomColour = (): string => {
-    const randomRed: Number = Math.floor((Math.random() * 256));
-    const randomGreen: Number = Math.floor((Math.random() * 256));
-    const randomBlue: Number = Math.floor((Math.random() * 256));
-
-    return `rgb(${randomRed}, ${randomGreen}, ${randomBlue})`;
-  };
-
-  const [randomColour, setRandomColour] = useState(generateRandomColour());
-
-  const myButtonPressed = () => {
-    Alert.alert('Text entered!');
-  };
-
   return (
-    <View style={{...styles.container, backgroundColor: randomColour}}>
-      <Text>🫰</Text>
-      <TouchableOpacity
-        onPress={() => setRandomColour(generateRandomColour)}
-        >
-          <Text style={styles.text}>Generate random background color</Text>
-      </TouchableOpacity>
-    </View>
+    <NativeBaseProvider>
+      <View style={styles.container}>
+        <Text>🫰</Text>
+        <Button style={styles.button}>
+          <Text>Ok!</Text>
+        </Button>
+      </View>
+    </NativeBaseProvider>
   );
 }
 
@@ -37,26 +22,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  inputText: {
-    width: 250,
-    height: 50,
-    borderWidth: 1,
-    borderRadius: 5,
-    borderColor: '#7d4a58',
-    marginTop: 30,
-    marginBottom: 10,
-    padding: 10,
-  },
-  text: {
-    marginTop: 30,
-    fontSize: 16,
-    color: '#11111f',
-    backgroundColor: '#ff94b2',
-    paddingVertical: 10,
-    paddingHorizontal: 40,
-    borderRadius: 10,
-    borderWidth: 2,
-    borderColor: '#fffffe',
+  button: {
+    padding: 8,
+    marginVertical: 12,
+    backgroundColor: 'pink',
   },
 });
 
@@ -150,4 +119,68 @@ export default function App() {
     </View>
   );
 }
+*/
+
+/**
+ * Background colour changer app.
+ */
+/*
+export default function App() {
+  //const [myText, setMyText] = useState('Generate random colour');
+
+  const generateRandomColour = (): string => {
+    const randomRed: Number = Math.floor((Math.random() * 256));
+    const randomGreen: Number = Math.floor((Math.random() * 256));
+    const randomBlue: Number = Math.floor((Math.random() * 256));
+
+    return `rgb(${randomRed}, ${randomGreen}, ${randomBlue})`;
+  };
+
+  const [randomColour, setRandomColour] = useState(generateRandomColour());
+
+  const myButtonPressed = () => {
+    Alert.alert('Text entered!');
+  };
+
+  return (
+    <View style={{...styles.container, backgroundColor: randomColour}}>
+      <Text>🫰</Text>
+      <TouchableOpacity
+        onPress={() => setRandomColour(generateRandomColour)}
+        >
+          <Text style={styles.text}>Generate random background color</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fffff1',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputText: {
+    width: 250,
+    height: 50,
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: '#7d4a58',
+    marginTop: 30,
+    marginBottom: 10,
+    padding: 10,
+  },
+  text: {
+    marginTop: 30,
+    fontSize: 16,
+    color: '#11111f',
+    backgroundColor: '#ff94b2',
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: '#fffffe',
+  },
+});
 */
